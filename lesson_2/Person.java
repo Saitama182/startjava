d22aca891078f0db45b60d51abab4542d19daada
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-06 15:00:00 +0300

    2 урок

---
+++ lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+   boolean maleGender = true;
+   String name = "Evgen";
+   double height = 1.93;
+   int weight = 90;
+   int age = 33;
+
+    public void go () {
+        System.out.println("идти");
+    }
+
+    public void sit () {
+        System.out.println("сидеть");
+    }
+
+    public void run () {
+        System.out.println("бежать");
+    }
+
+    public void speak () {
+        System.out.println("говорить");
+    }
+
+    public void learn () {
+        System.out.println("учить Java");
+    }
+
+}
